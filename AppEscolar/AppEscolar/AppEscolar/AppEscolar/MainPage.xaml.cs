349a3dc638f7a7107dd13b896b53dd6f42ae0b83
manoelcontatosi@gmail.com <manoelcontatosi@gmail.com>
2018-05-23 20:53:22 -0300

    AppShoolBr. Desenvolvimeto da arquitetura inicial do Ap.

---
+++ AppEscolar/AppEscolar/AppEscolar/AppEscolar/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using AppEscolar.Model;
+using AppEscolar.Service;
+using AppEscolar.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppEscolar
+{
+	public partial class MainPage : MasterDetailPage
+
+    {
+
+        private ObservableCollection<MasterPageItem> _menuLista;
+        public MainPage()
+		{
+            InitializeComponent();
+            ImgbackMenu.Source = ImageSource.FromResource("HenHouseDomotica.Resource.backMenu.jpg");
+            _menuLista = ItemService.GetMenuItens();
+            navigationDrawerList.ItemsSource = _menuLista;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(HomePage)));
+        }
+
+        private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = (MasterPageItem)e.SelectedItem;
+            //obtem o tipo de objeto 
+            Type pagina = item.TargetType;
+
+            //abre a pagina correspondente ao item selecionado
+            //Cria uma instância do tipo especificado usando o construtor
+            //que melhor se adequa ao parametro informado
+            Detail = new NavigationPage((Page)Activator.CreateInstance(pagina));
+            IsPresented = false;
+        }
+
+    }
+}
---
+++ AppEscolar/AppEscolar/AppEscolar/AppEscolar/Service/ItemService.cs
@@ -0,0 +1,31 @@
+﻿using AppEscolar.Model;
+using AppEscolar.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AppEscolar.Service
+{
+    public class ItemService
+    {
+        private static ObservableCollection<MasterPageItem> menuLista { get; set; }
+
+        public static ObservableCollection<MasterPageItem> GetMenuItens()
+        {
+            menuLista = new ObservableCollection<MasterPageItem>();
+            // Criando as paginas para navegação
+            // Definimos o titulo para o item
+            // o icone do lado esquerdo e a pagina que vamos abrir
+            var DesciplinasView = new MasterPageItem() { Title = "Desciplinas", TargetType = typeof(DisciplinaPage) };
+            var AddAlunoView = new MasterPageItem() { Title = "Cadastror Aluno", TargetType = typeof(AlunoPage) };
+            var LancarNotasView = new MasterPageItem() { Title = "Lançar Notas", TargetType = typeof(NotasPage) };
+            // Adicionando items no menuLista
+            menuLista.Add(DesciplinasView);
+            menuLista.Add(AddAlunoView);
+            menuLista.Add(LancarNotasView);
+            //retorna a lista de itens 
+            return menuLista;
+        }
+    }
+}
